443f95304387d7b7846aefd3d01bbb71e569c0b5
abdullahi samsudeen <abdullahisamsudeen@gmail.com>
2024-01-05 07:19:17 +0100

    commit

--- src/component/Student/pages/Password.jsx
+++ src/component/Student/pages/Password.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react'
 import Canvas from '../Canvas'
 import { Link, useNavigate } from 'react-router-dom'
+import Passwordrecovery from './Passwordrecovery'
 
 
 const Password = () => {
@@ -14,37 +15,7 @@ const Password = () => {
   return (
     <>
         <Canvas/>
-
-
-        <div className="col-lg-8 container border-light rounded shadow border-dark mt-5">
-        <div className='fw-bold'>
-            <div>CHANGE PASSWORD</div>
-            <br />
-            <div>
-            <p>Old Password</p>
-                <input className='w-100 rounded' type="text" />
-            </div>
-        </div>
-        <br />
-
-        <div>
-            <p className='fw-bold'>New Password</p>
-            <div>
-            <input className='w-100 rounded' type="text" />
-            </div>
-        <div>
-        <br />
-        <div>
-            <p className='fw-bold'>Confirm New Passcode</p>
-            <div>
-            <input className='w-100 rounded' type="text" />
-            </div>
-            <br />
-        </div>
-                <Link className='btn btn-dark my-2 text-decoration-none fw-bold' to="">SAVE CHANGES</Link>
-            </div>
-        </div>
-    </div>
+    <Passwordrecovery/>
     </>
   )
 }
